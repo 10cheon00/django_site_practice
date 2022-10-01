d4b69038b8d1a8d2c1934a5211498e599c18b08e
10cheon00 <10cheon00@gmail.com>
2022-10-01 18:28:30 +0900

    feat: Add favorate_race field on User model

--- users/models.py
+++ users/models.py
@@ -10,12 +10,19 @@ class User(AbstractBaseUser, PermissionsMixin):
         ("email", "Email"),
         ("kakao", "Kakao"),
     ]
+    RACE_LIST = [
+        ("terran", "Terran"),
+        ("zerg", "Zerg"),
+        ("protoss", "Protoss"),
+        ("random", "Random"),
+    ]
 
     registration_type = models.CharField(
         max_length=20, choices=USER_TYPE_CHOICES, default=USER_TYPE_CHOICES[0]
     )
     user_id = models.CharField(max_length=100, unique=True)
     clan_id = models.CharField(max_length=100, unique=True)
+    favorate_race = models.CharField(max_length=10, choices=RACE_LIST)
 
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
