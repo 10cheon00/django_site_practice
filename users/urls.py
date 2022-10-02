ce212f75f2ce070e42ba66c671bc12233e23d364
10cheon00 <10cheon00@gmail.com>
2022-10-02 21:21:09 +0900

    feat: Create EmailVerification
    When client posts form data what contains user information,
     server create user and sending verification email to user.
    When user click link what verification email includes,
     is_active fields has been changed to True.

--- users/urls.py
+++ users/urls.py
@@ -5,10 +5,16 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 from users.views import KakaoSignInView
 from users.views import KakaoSignUpView
+from users.views import VerifyEmailAPIView
+from users.views import EmailRegistrationAPIView
 
 urlpatterns = [
     path("signin/kakao", KakaoSignInView.as_view()),
     path("signup/kakao", KakaoSignUpView.as_view()),
+    path("registration/email", EmailRegistrationAPIView.as_view()),
+    path(
+        "registration/email/verify", VerifyEmailAPIView.as_view(), name="verify-email"
+    ),
     path("token", TokenObtainPairView.as_view()),
     path("token/refresh", TokenRefreshView.as_view()),
 ]
