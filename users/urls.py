078cf6f72b8a44cb4009423c8b763c3827b214d5
10cheon00 <10cheon00@gmail.com>
2022-10-03 14:09:32 +0900

    feat: Add name parameter in users/urls.py
    For testing, name parameter is required.

--- users/urls.py
+++ users/urls.py
@@ -3,21 +3,29 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
+from users.views import EmailRegistrationAPIView
 from users.views import KakaoLogInView
 from users.views import KakaoRegistrationView
 from users.views import PasswordLogInAPIView
 from users.views import VerifyEmailAPIView
-from users.views import EmailRegistrationAPIView
 
 
 urlpatterns = [
-    path("login/kakao", KakaoLogInView.as_view()),
-    path("registration/kakao", KakaoRegistrationView.as_view()),
-    path("login", PasswordLogInAPIView.as_view()),
-    path("registration/email", EmailRegistrationAPIView.as_view()),
+    path("login", PasswordLogInAPIView.as_view(), name="login"),
+    path("login/kakao", KakaoLogInView.as_view(), name="kakao-login"),
     path(
-        "registration/email/verify", VerifyEmailAPIView.as_view(), name="verify-email"
+        "registration/kakao", KakaoRegistrationView.as_view(), name="kakao-registration"
     ),
-    path("token", TokenObtainPairView.as_view()),
-    path("token/refresh", TokenRefreshView.as_view()),
+    path(
+        "registration/email",
+        EmailRegistrationAPIView.as_view(),
+        name="email-registration",
+    ),
+    path(
+        "registration/email/verify",
+        VerifyEmailAPIView.as_view(),
+        name="email-verification",
+    ),
+    path("token", TokenObtainPairView.as_view(), name="token-obtain"),
+    path("token/refresh", TokenRefreshView.as_view(), name="token-refresh"),
 ]
