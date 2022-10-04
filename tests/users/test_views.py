ba2b1c5fb1895e2026a4fc56886963e95938af52
10cheon00 <10cheon00@gmail.com>
2022-10-04 10:34:35 +0900

    refactor: Rename variable name in tests/users/test_views.py
    url -> registartion_url.
    form_data -> registration_form.

--- tests/users/test_views.py
+++ tests/users/test_views.py
@@ -12,7 +12,7 @@ from rest_framework.test import APITestCase
 class EmailRegistrationTest(APITestCase):
     def setUp(self):
         self.registration_url = reverse("registration")
-        self.form_data = {
+        self.registration_form = {
             "username": "user01",
             "password": "password01",
             "nickname": "nickname01",
@@ -24,7 +24,9 @@ class EmailRegistrationTest(APITestCase):
         get_user_model().objects.all().delete()
 
     def test_success_registration(self):
-        response = self.client.post(path=self.registration_url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue("access" in response.data)
         self.assertTrue("refresh" in response.data)
@@ -32,37 +34,45 @@ class EmailRegistrationTest(APITestCase):
     def test_fail_registration_with_wrong_email(self):
         """Registering with wrong email format will be fail."""
 
-        form_data = self.form_data
-        form_data["email"] = "wrong_email@wrong_email.com"
-        response = self.client.post(path=self.registration_url, data=form_data)
+        registration_form = self.registration_form
+        registration_form["email"] = "wrong_email@wrong_email.com"
+        response = self.client.post(path=self.registration_url, data=registration_form)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_fail_registration_with_wrong_favorate_race(self):
         """Registering with value what not allowed in choices will be fail."""
 
-        form_data = self.form_data
-        form_data["favorate_race"] = "wrong_race"
-        response = self.client.post(path=self.registration_url, data=form_data)
+        registration_form = self.registration_form
+        registration_form["favorate_race"] = "wrong_race"
+        response = self.client.post(path=self.registration_url, data=registration_form)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_fail_registration_with_already_exist_user(self):
         """Registering with already exist data will be fail."""
 
-        response = self.client.post(path=self.registration_url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        response = self.client.post(path=self.registration_url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_success_sending_verification_email_while_registration(self):
-        response = self.client.post(path=self.registration_url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         self.assertEqual(len(mail.outbox), 1)
         self.assertEqual(mail.outbox[0].subject, "Verify your email")
 
     def test_success_email_verification(self):
-        response = self.client.post(path=self.registration_url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         verification_url = mail.outbox[0].body.split("\n", 1)[1]
@@ -72,7 +82,9 @@ class EmailRegistrationTest(APITestCase):
     def test_fail_email_verification_with_wrong_token(self):
         """Verifying with wrong token will be fail"""
 
-        response = self.client.post(path=self.registration_url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         verification_url = mail.outbox[0].body.split("\n", 1)[1]
@@ -84,7 +96,9 @@ class EmailRegistrationTest(APITestCase):
     def test_fail_email_verification_with_expired_token(self):
         """Verifying with expired token will be failed"""
 
-        response = self.client.post(path=self.registration_url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         expired_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY0Nzc3NDM3LCJpYXQiOjE2NjQ3NzcxMzcsImp0aSI6IjA5YjJiNWUwY2FiMjQ3ZjRiOTg5MTQyOWIzOTczNjI1IiwidXNlcl9pZCI6MX0.N_i60m0deO9FHA0VQ662AVKywSF2KaXVjlOti7E35sE"
@@ -110,8 +124,8 @@ class KakaoRegistrationTest(APITestCase):
             }
 
     def setUp(self):
-        self.url = reverse("kakao-registration")
-        self.form_data = {
+        self.registration_url = reverse("kakao-registration")
+        self.registration_form = {
             "access_token": "token",
             "nickname": "sample",
             "favorate_race": "protoss",
@@ -123,7 +137,9 @@ class KakaoRegistrationTest(APITestCase):
     def test_success_kakao_registration(self, mock_kakao_api):
         mock_kakao_api.get = Mock(return_value=self.MockKakaoResponse())
 
-        response = self.client.post(path=self.url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue("access" in response.data)
         self.assertTrue("refresh" in response.data)
@@ -138,32 +154,38 @@ class KakaoRegistrationTest(APITestCase):
 
         mock_kakao_api.get = Mock(return_value=MockKakaoResponse())
 
-        form_data = self.form_data
-        form_data["access_token"] = "something wrong in access_token"
-        response = self.client.post(path=self.url, data=form_data)
+        registration_form = self.registration_form
+        registration_form["access_token"] = "something wrong in access_token"
+        response = self.client.post(path=self.registration_url, data=registration_form)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_fail_kakao_registration_with_wrong_favorate_race(self, mock_kakao_api):
         mock_kakao_api.get = Mock(return_value=self.MockKakaoResponse())
 
-        form_data = self.form_data
-        form_data["favorate_race"] = "wrong_race"
-        response = self.client.post(path=self.url, data=form_data)
+        registration_form = self.registration_form
+        registration_form["favorate_race"] = "wrong_race"
+        response = self.client.post(path=self.registration_url, data=registration_form)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_fail_kakao_registration_with_already_exist_user(self, mock_kakao_api):
         mock_kakao_api.get = Mock(return_value=self.MockKakaoResponse())
 
-        response = self.client.post(path=self.url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        response = self.client.post(path=self.url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_fail_kakao_registration_with_already_exist_nickname(self, mock_kakao_api):
         mock_kakao_api.get = Mock(return_value=self.MockKakaoResponse())
 
-        response = self.client.post(path=self.url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         class MockKakaoResponse(self.MockKakaoResponse):
@@ -174,7 +196,9 @@ class KakaoRegistrationTest(APITestCase):
                 }
 
         mock_kakao_api.get = Mock(return_value=MockKakaoResponse())
-        response = self.client.post(path=self.url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
@@ -183,8 +207,8 @@ class AuthenticationTest(APITestCase):
         self.registration_url = reverse("registration")
         self.authentication_url = reverse("login")
         self.credential = {"username": "sample", "password": "password"}
-        self.form_data = self.credential
-        self.form_data.update(
+        self.registration_form = self.credential
+        self.registration_form.update(
             {
                 "favorate_race": "zerg",
                 "email": "sample@email.com",
@@ -196,7 +220,9 @@ class AuthenticationTest(APITestCase):
         get_user_model().objects.all().delete()
 
     def test_success_authentication(self):
-        response = self.client.post(path=self.registration_url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         response = self.client.post(path=self.authentication_url, data=self.credential)
@@ -205,7 +231,9 @@ class AuthenticationTest(APITestCase):
         self.assertTrue("refresh" in response.data)
 
     def test_fail_authentication_with_wrong_credential(self):
-        response = self.client.post(path=self.registration_url, data=self.form_data)
+        response = self.client.post(
+            path=self.registration_url, data=self.registration_form
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         wrong_credential = {"username": "wrong_name", "password": "password"}
